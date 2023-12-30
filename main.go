784e81b0eb2ac8a4e7db111223e3da450e232290
sidpandagle <siddhant.pandagle1998@gmail.com>
2023-12-30 17:48:37 +0530

    Basic Crud done

--- main.go
+++ main.go
@@ -1,29 +1,25 @@
 package main
 
 import (
-	"os"
-
+    "fibgo/configs"
+    "fibgo/routes"
 	"github.com/gofiber/fiber/v2"
 )
 
-func getPort() string {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = ":3000"
-	} else {
-		port = ":" + port
-	}
-
-	return port
+func hello(c *fiber.Ctx) error{
+	return c.JSON(&fiber.Map{"data": "Hello from Fiber & mongoDB"})
 }
 
-func main() {
+func main(){
 	app := fiber.New()
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.JSON(fiber.Map{
-			"message": "Hello, Railway!",
-		})
-	})
 
-	app.Listen(getPort())
+    configs.ConnectDB()
+
+    routes.UserRoute(app)
+
+	app.Get("/", hello)
+
+	// app.Static("/", "./public") 
+
+	app.Listen(":3000")
 }
--- sample/sample.go
+++
@@ -1,7 +0,0 @@
-package sample
-
-import "fmt"
-
-func run(){
-	fmt.Println("Sample Run")
-}
