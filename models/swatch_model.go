38075adaacd80ec1ed0ab15848984f83ed112e26
spandagle <siddhant.pandagle1998@gmail.com>
2024-02-13 15:43:52 +0530

    Likes changed to intger

--- models/swatch_model.go
+++ models/swatch_model.go
@@ -3,9 +3,9 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Swatch struct {
-	Id        primitive.ObjectID   `json:"id,omitempty"`
-	Name      string               `json:"name,omitempty" validate:"required"`
-	Tags      string               `json:"tags,omitempty" validate:"required"`
-	CreatedAt string               `json:"createdAt,omitempty" validate:"required"`
-	Likes     primitive.Decimal128 `json:"likes,omitempty" validate:"required"`
+	Id        primitive.ObjectID `json:"id,omitempty"`
+	Name      string             `json:"name,omitempty" validate:"required"`
+	Tags      string             `json:"tags,omitempty" validate:"required"`
+	CreatedAt string             `json:"createdAt,omitempty" validate:"required"`
+	Likes     int64              `json:"likes,omitempty" validate:"required"`
 }
